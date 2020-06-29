3be00707281ab1a793af9ae06486186654545964
liugs <lgschn@qq.com>
2020-06-29 15:40:18 +0800

    Update RspConstracts.java

--- src/main/java/com/spring/ssm/Constracts/RspConstracts.java
+++ src/main/java/com/spring/ssm/Constracts/RspConstracts.java
@@ -5,8 +5,6 @@ package com.spring.ssm.Constracts;
  * @Description：返回值常量(这里用一句话描述这个类的作用)
  * @Author: liuguisheng
  * @Date: 2019/2/1 16:14:00
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class RspConstracts {
     public static String RSP_CODE_FAIL = "8888";
