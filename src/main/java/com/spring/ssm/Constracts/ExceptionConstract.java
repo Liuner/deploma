49858d0f2e0c5a65958251c724f0e57ab3255923
liugs <lgschn@qq.com>
2020-06-29 15:40:00 +0800

    Update ExceptionConstract.java

--- src/main/java/com/spring/ssm/Constracts/ExceptionConstract.java
+++ src/main/java/com/spring/ssm/Constracts/ExceptionConstract.java
@@ -5,8 +5,6 @@ package com.spring.ssm.Constracts;
  * @Description：异常常量
  * @Author: liuguisheng
  * @Date: 2019/3/6 13:31:45
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class ExceptionConstract {
     /**普通用户数据操作异常*/
