d2a4263458261d7d7dc67cc8951a23b69374ddb8
liugs <lgschn@qq.com>
2020-06-29 16:22:51 +0800

    Update RelGeneralJobCompanyService.java

--- src/main/java/com/spring/ssm/service/RelGeneralJobCompanyService.java
+++ src/main/java/com/spring/ssm/service/RelGeneralJobCompanyService.java
@@ -9,8 +9,6 @@ import com.spring.ssm.service.bo.RelGeneralJobCompanyRspBo;
  * @Description：普通用户 职位 公司 关系表 服务
  * @Author: liuguisheng
  * @Date: 2019/3/23 12:59:05
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public interface RelGeneralJobCompanyService {
 
