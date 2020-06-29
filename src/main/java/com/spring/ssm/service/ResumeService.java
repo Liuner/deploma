df9c78ac0b3d010d78fdb8d3c0de5ca2ab6e0c1d
liugs <lgschn@qq.com>
2020-06-29 16:23:37 +0800

    Update ResumeService.java

--- src/main/java/com/spring/ssm/service/ResumeService.java
+++ src/main/java/com/spring/ssm/service/ResumeService.java
@@ -9,8 +9,6 @@ import com.spring.ssm.service.bo.ResumeRspBo;
  * @Description：简历信息表服务
  * @Author: liuguisheng
  * @Date: 2019/3/25 16:32:16
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public interface ResumeService {
     /**
