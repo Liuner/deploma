c7de2c552d876adb3cbf3ba4a0e76e8d00c3718d
liugs <lgschn@qq.com>
2020-06-29 15:47:43 +0800

    Update ResumeMapper.java

--- src/main/java/com/spring/ssm/mapper/ResumeMapper.java
+++ src/main/java/com/spring/ssm/mapper/ResumeMapper.java
@@ -11,8 +11,6 @@ import java.util.List;
  * @Description：简历信息表 mapper
  * @Author: liuguisheng
  * @Date: 2019/3/25 15:57:40
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 @Repository
 public interface ResumeMapper {
