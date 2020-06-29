7a6cc7fc569b45d48b883d782ad0d6a2138d615d
liugs <lgschn@qq.com>
2020-06-29 16:18:36 +0800

    Update JobInfoRspBo.java

--- src/main/java/com/spring/ssm/service/bo/JobInfoRspBo.java
+++ src/main/java/com/spring/ssm/service/bo/JobInfoRspBo.java
@@ -5,8 +5,6 @@ package com.spring.ssm.service.bo;
  * @Description：职位信息 出参
  * @Author: liuguisheng
  * @Date: 2019/3/8 16:47:23
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class JobInfoRspBo extends BaseRspBo{
 
