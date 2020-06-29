e2841658bd8d6e8814d088567f0f9baa8d651b2b
liugs <lgschn@qq.com>
2020-06-29 16:23:14 +0800

    Update UserInfoService.java

--- src/main/java/com/spring/ssm/service/UserInfoService.java
+++ src/main/java/com/spring/ssm/service/UserInfoService.java
@@ -10,8 +10,6 @@ import java.util.List;
  * @Description：普通用户信息表 服务
  * @Author: liuguisheng
  * @Date: 2019/2/2 12:46:28
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public interface UserInfoService {
     UserInfoRspBo queryUserInfoById(Long id);
