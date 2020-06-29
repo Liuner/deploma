5792f86923100d3466b055f529aece4d42aebcc0
liugs <lgschn@qq.com>
2020-06-29 15:41:32 +0800

    Update QueryReceviedResumeRspBo.java

--- src/main/java/com/spring/ssm/comb/bo/QueryReceviedResumeRspBo.java
+++ src/main/java/com/spring/ssm/comb/bo/QueryReceviedResumeRspBo.java
@@ -7,8 +7,6 @@ import com.spring.ssm.service.bo.BaseRspBo;
  * @Description：查询收到的简历信息 出参
  * @Author: liuguisheng
  * @Date: 2019/3/26 9:08:39
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class QueryReceviedResumeRspBo extends BaseRspBo {
 
