8176915055b506b487d3f416b767fbc7715ac6a3
liugs <lgschn@qq.com>
2020-06-29 16:03:45 +0800

    Update AdminInfoReqBo.java

--- src/main/java/com/spring/ssm/service/bo/AdminInfoReqBo.java
+++ src/main/java/com/spring/ssm/service/bo/AdminInfoReqBo.java
@@ -7,8 +7,6 @@ import java.io.Serializable;
  * @Description：管理员信息表 入参
  * @Author: liuguisheng
  * @Date: 2019/2/11 18:09:14
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class AdminInfoReqBo implements Serializable {
 
