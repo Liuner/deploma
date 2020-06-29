1056374d46764bb46711d593bb5803707dfe3149
liugs <lgschn@qq.com>
2020-06-29 16:27:27 +0800

    Update UserInfoServiceImpl.java

--- src/main/java/com/spring/ssm/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/spring/ssm/service/impl/UserInfoServiceImpl.java
@@ -24,8 +24,6 @@ import java.util.List;
  * @Description：普通用户信息表 服务实现
  * @Author: liuguisheng
  * @Date: 2019/2/2 12:48:04
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 @Service
 public class UserInfoServiceImpl implements UserInfoService {
