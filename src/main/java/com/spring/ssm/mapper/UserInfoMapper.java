e8c74679cfc5bd2042e1d1d0bc937932634bc122
liugs <lgschn@qq.com>
2020-06-29 15:48:00 +0800

    Update UserInfoMapper.java

--- src/main/java/com/spring/ssm/mapper/UserInfoMapper.java
+++ src/main/java/com/spring/ssm/mapper/UserInfoMapper.java
@@ -11,8 +11,6 @@ import java.util.List;
  * @Description：普通用户信息表 mapper
  * @Author: liuguisheng
  * @Date: 2019/2/2 11:31:38
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 @Repository
 public interface UserInfoMapper {
