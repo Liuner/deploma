319d1555f982556b9b5723217f94d375b1cd12d5
liugs <lgschn@qq.com>
2020-06-29 16:19:02 +0800

    Update RelGeneralJobCompanyListRspBo.java

--- src/main/java/com/spring/ssm/service/bo/RelGeneralJobCompanyListRspBo.java
+++ src/main/java/com/spring/ssm/service/bo/RelGeneralJobCompanyListRspBo.java
@@ -7,8 +7,6 @@ import java.util.List;
  * @Description：关系表 列表查询 出参
  * @Author: liuguisheng
  * @Date: 2019/3/25 10:49:30
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class RelGeneralJobCompanyListRspBo extends BaseRspBo {
 
