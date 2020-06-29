57153b12bac4cc2236dac656b8087b789f89b4ba
liugs <lgschn@qq.com>
2020-06-29 15:46:59 +0800

    Update CompanyInfoMapper.java

--- src/main/java/com/spring/ssm/mapper/CompanyInfoMapper.java
+++ src/main/java/com/spring/ssm/mapper/CompanyInfoMapper.java
@@ -11,8 +11,6 @@ import java.util.List;
  * @Description：公司用户信息表 mapper
  * @Author: liuguisheng
  * @Date: 2019/2/7 15:04:57
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 @Repository
 public interface CompanyInfoMapper {
