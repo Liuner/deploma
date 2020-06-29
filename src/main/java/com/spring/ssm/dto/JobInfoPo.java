5359f26fdf9a1f304312429f8500888179c17965
liugs <lgschn@qq.com>
2020-06-29 15:45:16 +0800

    Update JobInfoPo.java

--- src/main/java/com/spring/ssm/dto/JobInfoPo.java
+++ src/main/java/com/spring/ssm/dto/JobInfoPo.java
@@ -7,8 +7,6 @@ import java.io.Serializable;
  * @Description：职位信息Po
  * @Author: liuguisheng
  * @Date: 2019/3/8 16:18:54
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class JobInfoPo implements Serializable {
 
