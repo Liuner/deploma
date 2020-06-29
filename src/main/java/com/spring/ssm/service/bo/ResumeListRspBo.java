8897417ef79635f4beaa5075e5cf6656d6c7a358
liugs <lgschn@qq.com>
2020-06-29 16:17:28 +0800

    Update ResumeListRspBo.java

--- src/main/java/com/spring/ssm/service/bo/ResumeListRspBo.java
+++ src/main/java/com/spring/ssm/service/bo/ResumeListRspBo.java
@@ -7,8 +7,6 @@ import java.util.List;
  * @Description：简历信息表 列表查询服务(这里用一句话描述这个类的作用)
  * @Author: liuguisheng
  * @Date: 2019/3/25 16:39:32
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class ResumeListRspBo extends BaseRspBo{
 
