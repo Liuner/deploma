8c29c199c5234fdf08c97e30956956c2bfd39d7b
liugs <lgschn@qq.com>
2020-06-29 16:19:30 +0800

    Update RelGeneralJobCompanyReqBo.java

--- src/main/java/com/spring/ssm/service/bo/RelGeneralJobCompanyReqBo.java
+++ src/main/java/com/spring/ssm/service/bo/RelGeneralJobCompanyReqBo.java
@@ -7,8 +7,6 @@ import java.io.Serializable;
  * @Description：普通用户 职位 公司关系表 入参
  * @Author: liuguisheng
  * @Date: 2019/3/23 12:51:23
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class RelGeneralJobCompanyReqBo implements Serializable {
 
