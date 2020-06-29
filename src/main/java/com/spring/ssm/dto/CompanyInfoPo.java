069565eac44fa9cda3012a4b29d024d7912bd177
liugs <lgschn@qq.com>
2020-06-29 15:44:59 +0800

    Update CompanyInfoPo.java

--- src/main/java/com/spring/ssm/dto/CompanyInfoPo.java
+++ src/main/java/com/spring/ssm/dto/CompanyInfoPo.java
@@ -8,8 +8,6 @@ import java.util.Date;
  * @Description：公司用户信息表 PO
  * @Author: liuguisheng
  * @Date: 2019/2/7 15:03:32
- * @Copyright:2019 www.tydic.com Inc. All rights reserved.
- * 注意：本内容仅限于北京天源迪科信息技术有限公司内部传阅，禁止外泄以及用于其他商业目的
  */
 public class CompanyInfoPo implements Serializable {
 
